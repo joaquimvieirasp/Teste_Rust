7879efcbc0863e32746c631169a14e53e6c72e73
jvsouza <jvsouza@atech.com.br>
2022-02-10 18:24:14 -0300

    Primeiro commit

---
+++ target/debug/deps/hello_cargo-ab67ca3ec9ebebea.d
@@ -0,0 +1,5 @@
+/home/joaquim/projects/hello_cargo/target/debug/deps/hello_cargo-ab67ca3ec9ebebea.rmeta: src/main.rs
+
+/home/joaquim/projects/hello_cargo/target/debug/deps/hello_cargo-ab67ca3ec9ebebea.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/joaquim/projects/hello_cargo/target/debug/hello_cargo: /home/joaquim/projects/hello_cargo/src/main.rs
